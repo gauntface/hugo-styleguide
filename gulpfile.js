e8676dc6f89088b94dbaa0bf920b2f4a776aa4d9
Matt Gaunt <mattgaunt@google.com>
2020-01-04 14:56:45 -0800

    Minor changes to work with new theme etc

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,20 @@
 const gulp = require('gulp');
 const path = require('path');
 const tsBrowser = require('@hopin/wbt-ts-browser'); 
+const css = require('@hopin/wbt-css'); 
 
-gulp.task('build-template', gulp.series(
+gulp.task('build', gulp.series(
   tsBrowser.gulpBuild('hopin.styleguide', {
     src: path.join(__dirname, 'static', 'ts'),
     dst: path.join(__dirname, 'static', 'js'),
+  }),
+
+  // .min.css files, minimized and variables in place
+  css.gulpBuild({
+    src: path.join(__dirname, 'static', 'css'),
+    dst: path.join(__dirname, 'static', 'css'),
+  }, {
+    importPaths: [path.join(__dirname, 'static', 'css')],
+    preserve: false,
   }),
 ))

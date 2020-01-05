28c1c5f1e3916009d1d997b471b542ab23f04d8e
Matt Gaunt <mattgaunt@google.com>
2020-01-05 14:43:56 -0800

    Adding src directory with a cleaner build process

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,49 @@
 const gulp = require('gulp');
 const path = require('path');
 const tsBrowser = require('@hopin/wbt-ts-browser'); 
-const css = require('@hopin/wbt-css'); 
+const css = require('@hopin/wbt-css');
+const clean = require('@hopin/wbt-clean');
 
-const themeSrc = path.join(__dirname, 'static');
+const themeSrc = path.join(__dirname, 'src');
+const themeDst = path.join(__dirname, 'build');
+
+gulp.task('clean', gulp.series(
+  clean.gulpClean({
+    src: themeSrc,
+    dst: themeDst,
+  }),
+))
+
+gulp.task('typescript', gulp.series(
+  tsBrowser.gulpBuild('hopin.styleguide', {
+    src: themeSrc,
+    dst: themeDst,
+  })
+))
+
+gulp.task('css', gulp.series(
+  css.gulpBuild({
+    src: themeSrc,
+    dst: themeDst,
+  }, {
+    importPaths: [themeSrc],
+  }),
+))
+
+gulp.task('copy', gulp.series(
+  () => {
+    return gulp.src(path.join(themeSrc, '**/*.html'))
+    .pipe(gulp.dest(themeDst));
+  }
+))
 
 gulp.task('build', gulp.series(
-  tsBrowser.gulpBuild('hopin.styleguide', {
-    src: path.join(themeSrc, 'ts'),
-    dst: path.join(themeSrc, 'js'),
-  }),
-
-  // .min.css files, minimized and variables in place
-  css.gulpBuild({
-    src: path.join(themeSrc, 'css'),
-    dst: path.join(themeSrc, 'css'),
-  }, {
-    importPaths: [path.join(themeSrc, 'css')],
-    preserve: false,
-  }),
+  'clean',
+  gulp.parallel(
+    'typescript',
+    'css',
+    'copy',
+  ),
 ))
 
 gulp.task('watch', () => gulp.watch([path.join(themeSrc, '**', '*')], {

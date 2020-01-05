3f0c956b28294fdc152e6bb2ad1ee297a652fcad
Matt Gaunt <mattgaunt@google.com>
2020-01-05 13:09:10 -0800

    Adding start of styleguide list page

--- gulpfile.js
+++ gulpfile.js
@@ -3,18 +3,24 @@ const path = require('path');
 const tsBrowser = require('@hopin/wbt-ts-browser'); 
 const css = require('@hopin/wbt-css'); 
 
+const themeSrc = path.join(__dirname, 'static');
+
 gulp.task('build', gulp.series(
   tsBrowser.gulpBuild('hopin.styleguide', {
-    src: path.join(__dirname, 'static', 'ts'),
-    dst: path.join(__dirname, 'static', 'js'),
+    src: path.join(themeSrc, 'ts'),
+    dst: path.join(themeSrc, 'js'),
   }),
 
   // .min.css files, minimized and variables in place
   css.gulpBuild({
-    src: path.join(__dirname, 'static', 'css'),
-    dst: path.join(__dirname, 'static', 'css'),
+    src: path.join(themeSrc, 'css'),
+    dst: path.join(themeSrc, 'css'),
   }, {
-    importPaths: [path.join(__dirname, 'static', 'css')],
+    importPaths: [path.join(themeSrc, 'css')],
     preserve: false,
   }),
 ))
+
+gulp.task('watch', () => gulp.watch([path.join(themeSrc, '**', '*')], {
+  ignoreInitial: false,
+}, gulp.series('build')))

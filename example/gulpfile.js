9d1422832d68afe54381765db9a65f77b6e08596
Matt Gaunt <mattgaunt@google.com>
2020-01-23 18:15:39 -0800

    Filter styleguide layouts

--- example/gulpfile.js
+++ example/gulpfile.js
@@ -2,6 +2,7 @@ const gulp = require('gulp');
 const {promisify} = require('util');
 const exec = promisify(require('child_process').exec);
 const spawn = require('child_process').spawn;
+const clean = require('@hopin/wbt-clean');
 const path = require('path');
 
 gulp.task('update-submodules',
@@ -13,6 +14,12 @@ gulp.task('update-submodules',
   )
 );
 
+gulp.task('clean-themes', gulp.series(
+  clean.gulpClean({
+    dst: path.join(__dirname, 'themes'),
+  }),
+))
+
 gulp.task('build-hopin-base-theme',async () => {
   const {stdout, stderr} = await exec(`npm run build-into-site`, {
     cwd: path.join(__dirname, 'themes-src', 'hopin-base-theme'),
@@ -20,9 +27,18 @@ gulp.task('build-hopin-base-theme',async () => {
   console.log(stdout, stderr);
 });
 
+gulp.task('copy-hopin-styleguide-theme', async () => {
+  return gulp.src(path.join(__dirname, '..', 'build', '**/*'))
+    .pipe(gulp.dest(path.join(__dirname, 'themes', 'hopin-styleguide-build')));
+});
+
 gulp.task('build', gulp.series(
   'update-submodules',
-  'build-hopin-base-theme',
+  'clean-themes',
+  gulp.series(
+    'copy-hopin-styleguide-theme',
+    'build-hopin-base-theme',
+  ),
 ));
 
 function startServer() {
@@ -41,12 +57,21 @@ gulp.task('hugo-server',
   gulp.series(startServer)
 );
 
+gulp.task('watch-hopin-styleguide-theme', () => {
+  gulp.watch(
+    [path.join(__dirname, '..', 'build', '**', '*')],
+    {},
+    gulp.series('copy-hopin-styleguide-theme'),
+  )
+})
+
 gulp.task('server',
   gulp.series(
     gulp.parallel(
       'build',
     ),
     gulp.parallel(
+      'watch-hopin-styleguide-theme',
       'hugo-server',
     )
   )
